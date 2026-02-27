e94207c668181de4dc31c8fb6dba645780753d54
lbj mac air m3 <1325349854@qq.com>
2026-02-27 23:29:48 +0800

    feat: 新增基于shadcn的主界面

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client"
 
 import "./index.css"
 import App from "./App.tsx"
+import { TooltipProvider } from "@/components/ui/tooltip"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
   </StrictMode>
 )
